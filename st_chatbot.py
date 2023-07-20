8be403378ffb087831221ba13addf4abf544031e
Mandar <111448138+mandarha@users.noreply.github.com>
2023-07-20 18:32:22 +0530

    Update st_chatbot.py

--- st_chatbot.py
+++ st_chatbot.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 prompt = st.chat_input("Say something")
 
+with st.chat_message("Here is my first Streamlit Chatbot")
 st.write(prompt)
 
-if prompt:
-  conn = st.experimental_connection('snowpark')
-  query = f"select llm_complete('(prompt)')"
-  response = conn.query(query).iloc(0,0)
+#if prompt:
+ # conn = st.experimental_connection('snowpark')
+  #query = f"select llm_complete('(prompt)')"
+  #response = conn.query(query).iloc(0,0)
 
-  
