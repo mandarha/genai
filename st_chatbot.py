8082a8cc303c60a3554745b3fa48e599da56d1e9
Mandar <111448138+mandarha@users.noreply.github.com>
2023-07-20 18:28:35 +0530

    Update st_chatbot.py

--- st_chatbot.py
+++ st_chatbot.py
@@ -4,7 +4,7 @@ prompt = st.chat_input("Say something")
 
 st.write(prompt)
 
-if prompt
+if prompt:
   conn = st.experimental_connection('snowpark')
   query = f"select llm_complete('(prompt)')
   response = conn.query(query).iloc(0,0)
